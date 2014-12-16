b7b29c1a9bd3f3a7888b63e0360aef1ba76bead5
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-12-16 02:28:00 +0100

    Adds test for all classes except SumInterval

--- mainParser.py
+++ mainParser.py
@@ -1,20 +1,29 @@
 import json
 import jsonClasses
 
-mapping = {
+appropriateConstructor = {
     'area': jsonClasses.Area,
     'labeled': jsonClasses.Labeled,
     'interval': jsonClasses.Interval,
     'sum_interval': jsonClasses.SumInterval,
-    'difference_interval': jsonClasses.DifferenceInterval,
+    'difference_interval': jsonClasses.TimeInterval,
     }
 
 jsonFile = open('config_new.json')
 json = json.load(jsonFile)
 
-for categoryName, category in json.items():
-    for entry in category['entries']
-        type = entry['type']
-        jsonObject = mapping[type](entry)
+start = 0
+end = 1390588135918
+deviceID = '92d47d9d-a600-4309-b340-b58314c2e429' 
 
+for categoryName, category in json.items():
+    if categoryName == "time":
+        continue
+    for entry in category['entries']:
+        type = entry['type']
+        if type == 'sum_interval':
+            continue
+        jsonObject = appropriateConstructor[type](entry)
+        data = jsonObject.getLabeledData(deviceID, start, end)
+        print(data)
 jsonFile.close()
