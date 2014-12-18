c5a5f4154052bb7e93dcf0f1c3cce62b034a5f40
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-12-18 15:09:15 +0100

    Changes timestamp

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from jsonParser import JSONParser
 
 jsonFileName = 'config_new.json'
 start = 0
-end = 1390810940000
+end = 1390510940000
 deviceID = '92d47d9d-a600-4309-b340-b58314c2e429'
 
 parser = JSONParser(jsonFileName)
