d1447b7d02dd4587112f234b3560ff6e2b1288ac
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-12-18 03:56:48 +0100

    Adds main program

---
+++ main.py
@@ -0,0 +1,9 @@
+from jsonParser import JSONParser
+
+jsonFileName = 'config_new.json'
+start = 0
+end = 1390810940000
+deviceID = '92d47d9d-a600-4309-b340-b58314c2e429'
+
+parser = JSONParser(jsonFileName)
+parser.generateArff(deviceID, start, end)
